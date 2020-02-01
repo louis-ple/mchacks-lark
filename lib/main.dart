3c661a99f6c67ee658379a750a8412a1bc3f6c91
Louis Plessis <louis.plessis@polymtl.ca>
2020-02-01 17:53:08 -0500

    Table list added

--- lib/main.dart
+++ lib/main.dart
@@ -116,19 +116,102 @@ class ServerScreenImpl extends State<ServerScreen> {
   }
 }
 
-class TableScreen extends StatelessWidget {
+class TableScreen extends StatefulWidget{
+  TableScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  Widget build (BuildContext ctxt) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Table List"),
+  _Tables createState() => _Tables();
+}
+
+// The base class for the different types of items the list can contain.
+class Table {
+  int id;
+  Table(this.id);
+}
+
+class _Tables extends State<TableScreen> {
+
+  Table table1 = new Table(1);
+  Table table2 = new Table(2);
+  Table table3 = new Table(3);
+
+  int _nb_tables = 0;
+  final List<Table> tables = <Table>[];
+  final TextEditingController eCtrl = TextEditingController();
+
+  void _showDialog() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Enter table number:"),
+          content: TextField(
+            controller: eCtrl,
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new MaterialButton(
+              child:  Text("Add"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addTable() {
+
+    _showDialog();
+
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _nb_tables++;
+    });
+  }
+
+  @override
+  Widget build (BuildContext context) {
+
+    final title = 'Table list';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
         ),
-        body: new Checkbox(
-            value: false,
-            onChanged: (bool newValue) {
-              Navigator.pop(ctxt); // Pop from stack
-            }
-        )
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Table $index',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            );
+          }),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: _addTable,
+          tooltip: 'Add a table',
+          child: Icon(Icons.add),
+        ),
+      ),
     );
   }
 }

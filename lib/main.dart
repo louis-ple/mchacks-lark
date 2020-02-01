5a6088726d9596e4f4d373ea90584aed1a7b2f2d
Katie <katkatz@bergen.org>
2020-02-01 15:02:16 -0500

    table and server pages created

--- lib/main.dart
+++ lib/main.dart
@@ -22,32 +22,62 @@ class FirstScreen extends StatelessWidget {
         appBar: new AppBar(
           title: new Text("Multi Page Application"),
         ),
+        body: Column(
+          children: <Widget>[
+            new Checkbox(
+                value: false,
+                onChanged: (bool newValue) {
+                  Navigator.push(
+                    ctxt,
+                    new MaterialPageRoute(builder: (ctxt) => new ServerScreen()),
+                  );
+                }
+            ),
+            new Checkbox(
+                value: false,
+                onChanged: (bool newValue) {
+                  Navigator.push(
+                    ctxt,
+                    new MaterialPageRoute(builder: (ctxt) => new TableScreen()),
+                  );
+                }
+            )
+          ]
+        ),
+    );
+  }
+}
+
+class ServerScreen extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Server List"),
+        ),
         body: new Checkbox(
             value: false,
             onChanged: (bool newValue) {
-              Navigator.push(
-                ctxt,
-                new MaterialPageRoute(builder: (ctxt) => new SecondScreen()),
-              );
+              Navigator.pop(ctxt); // Pop from stack
             }
         )
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
+class TableScreen extends StatelessWidget {
   @override
   Widget build (BuildContext ctxt) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Multi Page Application Page-1"),
-      ),
-      body: new Checkbox(
-          value: false,
-          onChanged: (bool newValue) {
-            Navigator.pop(ctxt); // Pop from stack
-          }
-      )
+        appBar: new AppBar(
+          title: new Text("Table List"),
+        ),
+        body: new Checkbox(
+            value: false,
+            onChanged: (bool newValue) {
+              Navigator.pop(ctxt); // Pop from stack
+            }
+        )
     );
   }
 }
